fb5d426df29f3513b80fdda378fe3d97d77f7248
masaki nakamura <gmk4n0n@gmail.com>
2023-02-15 05:26:48 +0900

    enhancing the way to sleep the system, using CountDownLatch to accomplish that

--- src/test/java/com/study/client/MoneyStreamingResponse.java
+++ src/test/java/com/study/client/MoneyStreamingResponse.java
@@ -3,7 +3,15 @@ package com.study.client;
 import com.study.models.Money;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MoneyStreamingResponse implements StreamObserver<Money> {
+    private CountDownLatch latch;
+
+    public MoneyStreamingResponse(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
     @Override
     public void onNext(Money money) {
         System.out.println("Received async: " + money.getValue());
@@ -12,10 +20,12 @@ public class MoneyStreamingResponse implements StreamObserver<Money> {
     @Override
     public void onError(Throwable t) {
         System.out.println(t.getMessage());
+        latch.countDown();
     }
 
     @Override
     public void onCompleted() {
         System.out.println("Server is Done!");
+        latch.countDown();
     }
 }
